b525b5bc27448e6889f6c532db5ac3720c260087
Olamide <olire96@gmail.com>
2023-03-26 20:56:32 +0100

    remove permission

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 #modify the config file with puppet
 file { '/root/.ssh/config':
   ensure  => file,
-  mode    => '0600',
   content => "Host 34.202.159.232\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",  
 }
